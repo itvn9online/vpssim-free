22fbfd0e5d7151cdcba43d6fd1af0154745cd81e
itvn9online <itvn9online@gmail.com>
2020-08-27 20:47:46 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>EchBay.com SSD Hosting</title>
+<style>
+input {
+	width: 99%;
+	text-align: center;
+	padding: 10px 0;
+	border: 0 none;
+	font-size: 14px;
+	outline: none;
+}
+</style>
+</head>
+<body>
+<input type="text" value="yum -y install wget ; wget –no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install" onClick="this.select();">
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,1 @@
+yum -y install wget ; wget –no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install
