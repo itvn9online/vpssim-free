8edafcb52de19be71db47b587da173559c0a0adc
itvn9online <itvn9online@gmail.com>
2020-08-28 05:12:06 +0700

    index

--- index.php
+++ index.php
@@ -15,6 +15,6 @@ input {
 </style>
 </head>
 <body>
-<input type="text" value="yum -y install wget ; wget –no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install" onClick="this.select();">
+<input type="text" value="yum -y install wget ; wget -–no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install" onClick="this.select();">
 </body>
 </html>
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-yum -y install wget ; wget –no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install
+yum -y install wget ; wget -–no-check-certificate https://vpssim.echbay.com/install ; chmod +x install ; bash install
