35a4a2b63f85e1aee141628fe31efe1820adf530
itvn9online <itvn9online@gmail.com>
2023-01-15 00:41:14 +0700

    check malware via logo gif

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -492,6 +492,10 @@ if [ $2 -lt $3 ]; then
 							# gui canh bao ve telegram
 							wget --no-check-certificate -O /dev/null "https://cloud.echbay.com/backups/has_malware?user=$4"
 						fi
+					elif [ -f $get_d/logo.gif ]; then
+						# file logo.gif nay cung do virus tao ra
+						# gui canh bao ve telegram
+						wget --no-check-certificate -O /dev/null "https://cloud.echbay.com/backups/has_malware?user=$4"
 					fi
 
 					echo "Begin rsync... Please wait..."
