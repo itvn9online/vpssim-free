7e155d65b4f79f590b63150c283b08505bed39f6
itvn9online <itvn9online@gmail.com>
2023-02-14 00:13:23 +0700

    elementor.3.11.0

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -8,7 +8,7 @@ fi
 # chuc nang cap nhat wordress cho toan bo website tren server
 for_classic_editor="classic-editor.1.6.2"
 for_yoast_seo="wordpress-seo.20.1"
-for_elementor="elementor.3.10.2"
+for_elementor="elementor.3.11.0"
 
 
 echoY() {
