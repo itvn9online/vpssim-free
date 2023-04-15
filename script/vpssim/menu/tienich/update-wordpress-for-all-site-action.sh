044e7302d3be4cce3d5b5375d368796042cc145f
Dao Quoc Dai <itvn9online@gmail.com>
2023-04-16 05:44:32 +0700

    classic-editor.1.6.3

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -6,9 +6,9 @@ fi
 
 
 # chuc nang cap nhat wordress cho toan bo website tren server
-for_classic_editor="classic-editor.1.6.2"
-for_yoast_seo="wordpress-seo.20.4"
-for_elementor="elementor.3.11.5"
+for_classic_editor="classic-editor.1.6.3"
+for_yoast_seo="wordpress-seo.20.5"
+for_elementor="elementor.3.12.1"
 
 
 echoY() {
