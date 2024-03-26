7830dbf0fc24a2f5d0a3dcdef1b3d72bca3e17b1
itvn9online <itvn9online@gmail.com>
2024-03-26 10:50:10 +0700

    sync

--- script/vpssim/menu/centos9/tmp/index.php
+++ script/vpssim/menu/centos9/tmp/index.php
@@ -1,3 +1,6 @@
 <p>Webcome to EchBay Hosting!</p>
 <p>Domain: <?php echo $_SERVER['HTTP_HOST']; ?></p>
+<p>Your IP: <?php echo (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : (isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'])); ?> (<?php echo $_SERVER['REMOTE_ADDR']; ?>)</p>
+<p>User agent: <?php echo (isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : ''); ?></p>
 <p>PHP version: <?php echo phpversion(); ?></p>
+<p>Server software: <?php echo $_SERVER['SERVER_SOFTWARE']; ?></p>
