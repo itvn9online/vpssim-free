84cb50cf16ddcaf182e4a0ebbcda0ef064771887
itvn9online <itvn9online@gmail.com>
2024-12-18 09:56:54 +0700

    woocommerce.9.5.1

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -8,7 +8,7 @@ fi
 # chuc nang cap nhat wordress cho toan bo website tren server
 for_classic_editor="classic-editor.1.6.7"
 for_elementor="elementor.3.26.0"
-for_woocommerce="woocommerce.9.4.3"
+for_woocommerce="woocommerce.9.5.1"
 for_yoast_seo="wordpress-seo.24.0"
 
 
