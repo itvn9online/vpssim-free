73805be07ce178f131b6d57fefbb8435b524e534
Dao Dai <itvn9online@gmail.com>
2024-12-26 09:56:33 +0700

    elementor.3.26.3

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -7,7 +7,7 @@ fi
 
 # chuc nang cap nhat wordress cho toan bo website tren server
 for_classic_editor="classic-editor.1.6.7"
-for_elementor="elementor.3.26.2"
+for_elementor="elementor.3.26.3"
 for_woocommerce="woocommerce.9.5.1"
 for_yoast_seo="wordpress-seo.24.1"
 
