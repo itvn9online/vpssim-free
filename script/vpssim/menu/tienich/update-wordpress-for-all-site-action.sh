9d5e71ebe34a12f20ca25bb3bc2c52b7b182a77f
Dao Dai <itvn9online@gmail.com>
2025-01-23 05:20:01 +0700

    woocommerce.9.6.0

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -7,9 +7,9 @@ fi
 
 # chuc nang cap nhat wordress cho toan bo website tren server
 for_classic_editor="classic-editor.1.6.7"
-for_elementor="elementor.3.26.4"
-for_woocommerce="woocommerce.9.5.2"
-for_yoast_seo="wordpress-seo.24.2"
+for_elementor="elementor.3.27.0"
+for_woocommerce="woocommerce.9.6.0"
+for_yoast_seo="wordpress-seo.24.3"
 
 
 echoY() {
