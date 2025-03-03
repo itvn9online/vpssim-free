0172d8666397e2d136cf9629404f1fb5ea458cd7
Dao Dai <itvn9online@gmail.com>
2025-03-04 04:02:19 +0700

    seo-by-rank-math.1.0.239

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -8,7 +8,7 @@ fi
 # chuc nang cap nhat wordress cho toan bo website tren server
 for_classic_editor="classic-editor.1.6.7"
 for_elementor="elementor.3.27.6"
-for_rankmath="seo-by-rank-math.1.0.238"
+for_rankmath="seo-by-rank-math.1.0.239"
 for_woocommerce="woocommerce.9.7.0"
 for_yoast_seo="wordpress-seo.24.5"
 
