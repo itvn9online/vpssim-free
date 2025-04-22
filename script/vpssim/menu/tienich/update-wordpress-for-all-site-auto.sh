50e65a126cb2668cd696217cae56995ff2eb0cf8
itvn9online <itvn9online@gmail.com>
2025-04-22 07:01:45 +0700

    sync

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-auto.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-auto.sh
@@ -7,8 +7,8 @@ fi
 cd ~
 
 /usr/bin/echo "# create file" > /tmp/server_wp_all_update
-/usr/bin/echo "root_dir=\"www\"" >> /tmp/server_wp_all_update
-# /usr/bin/echo "MaxCheck=3" >> /tmp/server_wp_all_update
+/usr/bin/echo "root_dir=\"/www/wwwroot\"" >> /tmp/server_wp_all_update
+/usr/bin/echo "MaxCheck=3" >> /tmp/server_wp_all_update
 # kiem tra da dung file htaccess cua WGR chua -> chi danh cho code cua WGR
 /usr/bin/echo "checkWgrCode=0" >> /tmp/server_wp_all_update
 # tắt xmlrpc để nhẹ web và tăng bảo mật
