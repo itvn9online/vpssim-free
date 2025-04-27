ac7d84864fce631b457cf0f6072c4c4750a39a77
itvn9online <itvn9online@gmail.com>
2025-04-27 13:16:13 +0700

    sync

---
+++ aapanel/chown-user-ini.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# file nay duoc luu lai code cua echbay.com
+# /echbay.com/bash-script/chown-user-ini.sh
+
+# chuyen ve root truoc khi chay lenh
+cd ~
+
+#
+if [ -d /www/wwwroot ]; then
+cd ~
+/usr/bin/chown -R www:www /www/wwwroot/
+/usr/bin/chown root:root /www/wwwroot/
+cd /www/wwwroot
+/usr/bin/find . -type f -name '*.user.ini' -exec /usr/bin/chmod 644 {} \;
+/usr/bin/find . -type f -name '*.user.ini' -exec /usr/bin/chown root:root {} \;
+fi
---
+++ aapanel/cronjob-10m.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# file nay duoc luu lai code cua echbay.com
+# /echbay.com/bash-script/create-user-ini-if-not-exist.sh
+
+# chuyen ve root truoc khi chay lenh
+cd ~
+
+# lay ngay thang nam hien tai
+curDate=$(/usr/bin/date +%Y-%m-%d)
+curDate="/tmp/cronjob-10m-"$curDate".log"
+echo "curDate: "$curDate
+
+# lay ngay thang nam hom qua
+# yesterdayDate=$(/usr/bin/date -d "1 days ago" +%Y-%m-%d)
+# yesterdayDate="/tmp/cronjob-10m-"$yesterdayDate".log"
+# echo "yesterdayDate: "$yesterdayDate
+
+# Memcached Daemon
+btpython /www/server/panel/script/restart_services.py memcached
+
+# Redis Daemon
+btpython /www/server/panel/script/restart_services.py redis
+
+# Nginx Daemon
+btpython /www/server/panel/script/restart_services.py nginx
+
+# Mysql Daemon
+btpython /www/server/panel/script/restart_services.py mysql
+
+# nếu ko có file log thì tạo mới
+if [ ! -f $curDate ]; then
+
+# xoa file log cu neu co
+/usr/bin/rm -rf /tmp/cronjob-10m-*.log
+
+# tạo file log moi, moi ngay chi chay 1 lan
+/usr/bin/echo "# create file" > $curDate
+
+# Domain SSL Renew Let's Encrypt Certificate
+/www/server/panel/pyenv/bin/python3 -u /www/server/panel/class/acme_v2.py --renew_v3=1
+
+# Renew Let's Encrypt Certificate
+# /www/server/panel/pyenv/bin/python3 -u /www/server/panel/class/acme_v2.py --renew_v3=1
+
+fi
