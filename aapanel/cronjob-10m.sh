52134edd1038a9a148e77ca7f6106f26ed850790
itvn9online <itvn9online@gmail.com>
2025-04-27 13:21:07 +0700

    sync

--- aapanel/cronjob-10m.sh
+++ aapanel/cronjob-10m.sh
@@ -6,15 +6,6 @@
 # chuyen ve root truoc khi chay lenh
 cd ~
 
-# lay ngay thang nam hien tai
-curDate=$(/usr/bin/date +%Y-%m-%d)
-curDate="/tmp/cronjob-10m-"$curDate".log"
-echo "curDate: "$curDate
-
-# lay ngay thang nam hom qua
-# yesterdayDate=$(/usr/bin/date -d "1 days ago" +%Y-%m-%d)
-# yesterdayDate="/tmp/cronjob-10m-"$yesterdayDate".log"
-# echo "yesterdayDate: "$yesterdayDate
 
 # Memcached Daemon
 btpython /www/server/panel/script/restart_services.py memcached
@@ -28,6 +19,17 @@ btpython /www/server/panel/script/restart_services.py nginx
 # Mysql Daemon
 btpython /www/server/panel/script/restart_services.py mysql
 
+
+# lay ngay thang nam hien tai
+curDate=$(/usr/bin/date +%Y-%m-%d)
+curDate="/tmp/cronjob-10m-"$curDate".log"
+echo "curDate: "$curDate
+
+# lay ngay thang nam hom qua
+# yesterdayDate=$(/usr/bin/date -d "1 days ago" +%Y-%m-%d)
+# yesterdayDate="/tmp/cronjob-10m-"$yesterdayDate".log"
+# echo "yesterdayDate: "$yesterdayDate
+
 # nếu ko có file log thì tạo mới
 if [ ! -f $curDate ]; then
 
@@ -36,11 +38,16 @@ if [ ! -f $curDate ]; then
 
 # tạo file log moi, moi ngay chi chay 1 lan
 /usr/bin/echo "# create file" > $curDate
+/usr/bin/echo $(date) >> $curDate
 
 # Domain SSL Renew Let's Encrypt Certificate
 /www/server/panel/pyenv/bin/python3 -u /www/server/panel/class/acme_v2.py --renew_v3=1
 
 # Renew Let's Encrypt Certificate
 # /www/server/panel/pyenv/bin/python3 -u /www/server/panel/class/acme_v2.py --renew_v3=1
+
+else
+
+echo "File log da ton tai, khong tao moi"
 
 fi
