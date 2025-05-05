f0f3b1cf3d39710036f101bf9f2f4a6ce46e52dd
itvn9online <itvn9online@gmail.com>
2025-05-05 14:58:20 +0700

    wordpress-seo.25.0

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -10,7 +10,7 @@ for_classic_editor="classic-editor.1.6.7"
 for_elementor="elementor.3.28.4"
 for_rankmath="seo-by-rank-math.1.0.243"
 for_woocommerce="woocommerce.9.8.3"
-for_yoast_seo="wordpress-seo.24.9"
+for_yoast_seo="wordpress-seo.25.0"
 
 
 echoY() {
