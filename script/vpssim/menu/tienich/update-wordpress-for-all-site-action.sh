541324fde89d9ea067247ee04a03d6632100f877
itvn9online <itvn9online@gmail.com>
2025-06-18 08:19:23 +0700

    woocommerce.9.9.4

--- script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
+++ script/vpssim/menu/tienich/update-wordpress-for-all-site-action.sh
@@ -9,8 +9,8 @@ fi
 for_classic_editor="classic-editor.1.6.7"
 for_elementor="elementor.3.29.2"
 for_rankmath="seo-by-rank-math.1.0.246"
-for_woocommerce="woocommerce.9.9.3"
-for_yoast_seo="wordpress-seo.25.2"
+for_woocommerce="woocommerce.9.9.4"
+for_yoast_seo="wordpress-seo.25.3"
 
 
 echoY() {
